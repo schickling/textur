cbdfd99b38119cbd7189d4eee1cbf035be4a5477
Benjamin Wanner <benni@benjamin-wanner.de>
2014-01-17 14:04:49 +0100

    Added helper genererateRandomArray

--- helper.js
+++ helper.js
@@ -3,3 +3,16 @@ window.hexColorForValue = function(val) {
 
 	return '#' + intensityString + intensityString + intensityString;
 }
+
+window.generateRandomArray = function(seed, height, width) {
+	var randomValues = new Array(height);
+
+	for(i = 0; i < height; i++) {
+		randomValues[i] = new Array(width);
+		for(int j = 0; j < width; j++) {
+			randomValues[i][j] = Math.random();
+		}
+	}
+
+	return randomValues;
+}
