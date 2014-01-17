ff7aea86db4d5b28593129147bbfaaa992072363
Johannes Schickling <schickling.j@gmail.com>
2014-01-17 14:04:00 +0100

    Random values

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 var canvasEl = document.getElementById('canvas'),
-	context = canvasEl.getContext('2d');
+	context = canvasEl.getContext('2d'),
+	width = 0.8 * window.innerWidth,
+	height = window.innerHeight;
 
-canvasEl.style.width = 0.8 * window.innerWidth;
-canvasEl.style.height = window.innerHeight;
+canvasEl.width = width;
+canvasEl.height = height;
 
-
-// test
-context.fillStyle = '#FF0000';
-context.fillRect(0,0,150,75);
+for (var y = 0; y < height; y++) {
+	for (var x = 0; x < width; x++) {
+		var id = context.createImageData(1, 1);
+		var d = id.data;
+		var rand = Math.random() * 255;
+		d[0] = rand;
+		d[1] = rand;
+		d[2] = rand;
+		d[3] = 255;
+		context.putImageData(id, x, y);
+	}
+}
