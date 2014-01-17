c652e38b510d0990da17e91693e41561d65377c4
Johannes Schickling <schickling.j@gmail.com>
2014-01-17 13:27:12 +0100

    rect

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-var canvasEl = document.getElementById('#canvas');
+var canvasEl = document.getElementById('canvas'),
+	context = canvasEl.getContext('2d');
 
 canvasEl.style.width = 0.8 * window.innerWidth;
 canvasEl.style.height = window.innerHeight;
+
+
+// test
+context.fillStyle = '#FF0000';
+context.fillRect(0,0,150,75);
