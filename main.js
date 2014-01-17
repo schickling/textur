e586c2bb4e10cf6d24318f1153336b4d2f5c8523
Johannes Schickling <schickling.j@gmail.com>
2014-01-17 14:28:27 +0100

    Generate button

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 var canvasEl = document.getElementById('canvas'),
 	context = canvasEl.getContext('2d'),
+	drawEl = document.getElementById('draw'),
 	width = 0.8 * window.innerWidth,
 	height = window.innerHeight;
 
-canvasEl.width = width;
-canvasEl.height = height;
+function init () {
+	canvasEl.width = width;
+	canvasEl.height = height;
+	initListeners();
+}
 
-randomValues = generateRandomArray(0, width, height);
+function initListeners () {
+	drawEl.addEventListener('click', draw);
+}
 
-for (var y = 0; y < height; y++) {
-	for (var x = 0; x < width; x++) {
-		var pixel = context.createImageData(1, 1),
-		rand = randomValues[y][x] * 255;
-		pixel.data[0] = rand;
-		pixel.data[1] = rand;
-		pixel.data[2] = rand;
-		pixel.data[3] = 255;
-		context.putImageData(pixel, x, y);
+function draw() {
+	context.clearRect(0, 0, width, height);
+	randomValues = generateRandomArray(0, width, height);
+	for (var y = 0; y < height; y++) {
+		for (var x = 0; x < width; x++) {
+			var pixel = context.createImageData(1, 1),
+				rand = randomValues[y][x] * 255;
+			pixel.data[0] = rand;
+			pixel.data[1] = rand;
+			pixel.data[2] = rand;
+			pixel.data[3] = 255;
+			context.putImageData(pixel, x, y);
+		}
 	}
 }
+
+init();
